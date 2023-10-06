9cdaff3930cb50a01ab9b2a23e2b50b45ba5dead
BowTiedPickle <92699017+BowTiedPickle@users.noreply.github.com>
2023-10-06 19:22:04 +0000

    feat: init protocol deploy script

---
+++ DEPLOYMENT.md
@@ -0,0 +1,22 @@
+# Deploying Protocol
+
+The following steps must be taken, in order, for the protocol to be ready to use.
+
+## Deploy contracts
+
+1. Deploy Vault
+2. Deploy VaultErrorController
+3. Deploy VaultUtils
+4. Deploy USDG
+5. Deploy PriceFeed
+    TODO: Investigate
+6. Deploy ShortsTracker
+    TODO: Investigate
+7. Deploy GLP
+8. Deploy Router
+
+## Configure contracts
+
+1. vault: Initialize the vault
+2. vault: Set the VaultUtils address
+3. vault: Set the ErrorController address
---
+++ scripts/deploy/protocol.js
@@ -0,0 +1,232 @@
+const { ethers } = require("hardhat")
+const readline = require('readline');
+const fs = require('fs');
+
+let deploymentInfo = require("./deploymentConfig.json");
+
+function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+}
+
+async function main() {
+    // Display the current network name and connected signer, and ask for y/n to proceed
+    await ethers.provider.ready
+    const network = await ethers.provider.getNetwork()
+    const chainId = network.chainId
+    console.log(`Network: ${network.name} - Chain ID: ${chainId}`)
+    const signer = await ethers.getSigner()
+    console.log(`Signer: ${await signer.getAddress()}`)
+    const response = await askQuestion("Do you wish to continue? (y/n) ")
+    if (response !== "y") {
+        return
+    }
+
+    // Deploy Vault
+    let vault;
+    const Vault = await ethers.getContractFactory("Vault")
+    if (deploymentInfo["Vault"].address == "") {
+        vault = await Vault.deploy()
+        await vault.deployed()
+        deploymentInfo["Vault"].address = vault.address
+    } else {
+        vault = Vault.attach(deploymentInfo["Vault"].address)
+    }
+    console.log(`Vault: ${vault.address}`)
+
+    // Deploy VaultErrorController
+    let vaultErrorController;
+    const VaultErrorController = await ethers.getContractFactory("VaultErrorController")
+    if (deploymentInfo["VaultErrorController"].address == "") {
+        vaultErrorController = await VaultErrorController.deploy()
+        await vaultErrorController.deployed()
+        deploymentInfo["VaultErrorController"].address = vaultErrorController.address
+    } else {
+        vaultErrorController = VaultErrorController.attach(deploymentInfo["VaultErrorController"].address)
+    }
+    console.log(`VaultErrorController: ${vaultErrorController.address}`)
+
+    // Deploy VaultUtils
+    let vaultUtils;
+    const VaultUtils = await ethers.getContractFactory("VaultUtils")
+    if (deploymentInfo["VaultUtils"].address == "") {
+        vaultUtils = await VaultUtils.deploy(vault.address)
+        await vaultUtils.deployed()
+        deploymentInfo["VaultUtils"].address = vaultUtils.address
+        deploymentInfo["VaultUtils"].constructorArgs = [vault.address]
+    } else {
+        vaultUtils = VaultUtils.attach(deploymentInfo["VaultUtils"].address)
+    }
+    console.log(`VaultUtils: ${vaultUtils.address}`)
+
+    // Deploy USDG
+    let usdg;
+    const USDG = await ethers.getContractFactory("USDG")
+    if (deploymentInfo["USDG"].address == "") {
+        usdg = await USDG.deploy(vault.address)
+        await usdg.deployed()
+        deploymentInfo["USDG"].address = usdg.address
+        deploymentInfo["USDG"].constructorArgs = [vault.address]
+    } else {
+        usdg = USDG.attach(deploymentInfo["USDG"].address)
+    }
+    console.log(`USDG: ${usdg.address}`)
+
+    // Deploy PriceFeed
+    let priceFeed;
+    const PriceFeed = await ethers.getContractFactory("PriceFeed")
+    if (deploymentInfo["PriceFeed"].address == "") {
+        priceFeed = await PriceFeed.deploy()
+        await priceFeed.deployed()
+        deploymentInfo["PriceFeed"].address = priceFeed.address
+    } else {
+        priceFeed = PriceFeed.attach(deploymentInfo["PriceFeed"].address)
+    }
+    console.log(`PriceFeed: ${priceFeed.address}`)
+
+    // Deploy ShortsTracker
+    let shortsTracker;
+    const ShortsTracker = await ethers.getContractFactory("ShortsTracker")
+    if (deploymentInfo["ShortsTracker"].address == "") {
+        shortsTracker = await ShortsTracker.deploy(vault.address)
+        await shortsTracker.deployed()
+        deploymentInfo["ShortsTracker"].address = shortsTracker.address
+        deploymentInfo["ShortsTracker"].constructorArgs = [vault.address]
+    } else {
+        shortsTracker = ShortsTracker.attach(deploymentInfo["ShortsTracker"].address)
+    }
+    console.log(`ShortsTracker: ${shortsTracker.address}`)
+
+    // Deploy GLP
+    let glp;
+    const GLP = await ethers.getContractFactory("GLP")
+    if (deploymentInfo["GLP"].address == "") {
+        glp = await GLP.deploy()
+        await glp.deployed()
+        deploymentInfo["GLP"].address = glp.address
+    } else {
+        glp = GLP.attach(deploymentInfo["GLP"].address)
+    }
+    console.log(`GLP: ${glp.address}`)
+
+    // Deploy WETH
+    let weth;
+    const WETH = await ethers.getContractFactory("WETH")
+    if (deploymentInfo["WETH"].address == "") {
+        weth = await WETH.deploy("WETH", "WETH", 18)
+        await weth.deployed()
+        deploymentInfo["WETH"].address = weth.address
+    } else {
+        weth = WETH.attach(deploymentInfo["WETH"].address)
+    }
+    console.log(`WETH: ${weth.address}`)
+
+    // Deploy Router
+    let router;
+    const Router = await ethers.getContractFactory("Router")
+    if (deploymentInfo["Router"].address == "") {
+        router = await Router.deploy(vault.address, usdg.address, weth.address)
+        await router.deployed()
+        deploymentInfo["Router"].address = router.address
+        deploymentInfo["Router"].constructorArgs = [vault.address, usdg.address, weth.address]
+    } else {
+        router = Router.attach(deploymentInfo["Router"].address)
+    }
+    console.log(`Router: ${router.address}`)
+
+    // Deploy GLPManager
+    let glpManager;
+    const GLPManager = await ethers.getContractFactory("GlpManager")
+    if (deploymentInfo["GlpManager"].address == "") {
+        glpManager = await GLPManager.deploy(vault.address, usdg.address, glp.address, shortsTracker.address, 30)
+        await glpManager.deployed()
+        deploymentInfo["GlpManager"].address = glpManager.address
+        deploymentInfo["GlpManager"].constructorArgs = [vault.address, usdg.address, glp.address, shortsTracker.address, 30]
+    } else {
+        glpManager = GLPManager.attach(deploymentInfo["GlpManager"].address)
+    }
+    console.log(`GlpManager: ${glpManager.address}`)
+
+    // Deploy OrderBook
+    let orderBook;
+    const OrderBook = await ethers.getContractFactory("OrderBook")
+    if (deploymentInfo["OrderBook"].address == "") {
+        orderBook = await OrderBook.deploy()
+        await orderBook.deployed()
+        deploymentInfo["OrderBook"].address = orderBook.address
+    } else {
+        orderBook = OrderBook.attach(deploymentInfo["OrderBook"].address)
+    }
+    console.log(`OrderBook: ${orderBook.address}`)
+
+    // Deploy PositionRouter - TODO FIX LIBRARIES
+    let positionRouter;
+    const PositionRouter = await ethers.getContractFactory("PositionRouter")
+    if (deploymentInfo["PositionRouter"].address == "") {
+        positionRouter = await PositionRouter.deploy(vault.address, router.address, weth.address, shortsTracker.address, 500, 500)
+        await positionRouter.deployed()
+        deploymentInfo["PositionRouter"].address = positionRouter.address
+        deploymentInfo["PositionRouter"].constructorArgs = [vault.address, router.address, weth.address, shortsTracker.address, 500, 500]
+    } else {
+        positionRouter = PositionRouter.attach(deploymentInfo["PositionRouter"].address)
+    }
+    console.log(`PositionRouter: ${positionRouter.address}`)
+
+    // -------------------------------
+    // POSTDEPLOYMENT CONFIGURATION
+    // -------------------------------
+
+    // Configure the vault
+    console.log("Configuring vault...")
+    if (deploymentInfo["Vault"].configure == true) {
+        await vault.initialize(
+            router.address,
+            usdg.address,
+            priceFeed.address,
+            500,
+            500,
+            100
+        )
+        await vault.setVaultUtils(vaultUtils.address)
+        await vault.setErrorController(vaultErrorController.address)
+    }
+
+
+
+    // -------------------------------
+    // POSTDEPLOYMENT ADMIN
+    // -------------------------------
+
+    // Save deployment info
+    if (!fs.existsSync(`deployments/${network.chainId}`)) {
+        fs.mkdirSync(`deployments/${network.chainId}`);
+    }
+    const filename = `deployments/${network.chainId}/deployment-${Date.now()}.json`
+    fs.writeFileSync(filename, JSON.stringify(deploymentInfo, null, 4))
+
+    // Verify contracts
+    for (const contractName in deploymentInfo) {
+        if (deploymentInfo[contractName].address != "" && deploymentInfo[contractName].verify == true) {
+            await run("verify:verify", {
+                address: deploymentInfo[contractName].address,
+                constructorArguments: deploymentInfo[contractName].constructorArgs,
+            })
+        } else {
+            console.log(`Skipping ${contractName} verification`)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error)
+        process.exit(1)
+    })
